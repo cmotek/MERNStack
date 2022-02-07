3a67e6c503cf3da616a9719e05fc3da537bd9dc1
unknown <cmotek1122@gmail.com>
2022-02-06 21:24:21 -0600

    secondupdate02062022

--- server.js
+++ server.js
@@ -1,4 +1,48 @@
+const { response } = require('express');
 var express = require('express');
 var app = express();
 app.set('port', 3000);
 app.listen(3000);
+
+// var server = app.listen(3000, funtion() {
+//    debug('Express server listening on port ' + server.address().port )
+//});
+
+app.get('/about', function(req, res) {
+    res.send("About page");
+});
+
+app.get('/news', function(req, res) {
+    res.send("News page");
+});
+
+app.route('/customer')
+    .get(function(req, res) {res.send('Get a customer');})
+    .post(function(req, res) {res.send('Add a customer');})
+    .put(function(req, res) {res.send('Update a customer');})
+
+//May have to use Advanced REST client to test the above *POSTMAN is common*
+
+app.all('/test', function(req, res) {
+    res.type('text/plain');
+    res.send('This a test');
+});
+
+app.use(function(req, res) {
+    res.type('type/plain');
+    res.status(404);
+    res.send('404 - Not found');
+})
+
+//not sure the use function works 
+
+app.get('/news/categories/:category', function(req, res) {
+    res.send(category);
+    console.log('Your category was ' + req.params.category);
+});
+
+app.get('/news/categories', function(req, res) {
+    res.send('Categories');
+    //console.log('Your category was ' + req.params.category);
+});
+
