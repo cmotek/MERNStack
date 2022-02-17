b7031b73d7c8a27f11a331cc9b27a4a51a1f3ed9
unknown <cmotek1122@gmail.com>
2022-02-17 13:34:30 -0600

    update02172022

--- server93.js
+++ server93.js
@@ -5,16 +5,52 @@ app.set('port', 3000);
 app.listen(3000);
 
 
-app.use('/', function(req, res, next) {
-    res.send("Worked!");
-    next();
+//The below works fine on first try
+var routerA = express.Router();
+var routerB = express.Router();
+
+routerA.get('/weather', function(req, res) {
+    res.send("Weather!");
 });
+routerA.get('/sports', function(req, res) {
+    res.send("Sports!");
+});
+
+routerB.get('/tech', function(req, res) {
+    res.send("Tech!");
+});
+routerB.get('/art', function(res, send) {
+    res.send("Art!");
+});
+
+app.use('/news', routerA);
+app.use('/blog', routerB);
+
+
+//Could not get below param and get functions to work will have to return, may require other routings
+
+//app.param('id', function(req, res, next, value) {
+//    console.log("someone queried id " + value);
+//    if (value != 99)
+//    next()
+//});
+
+//app.get('productions/:category/:id', function(req, res) {
+//    console.log(req.params[0] + req.params[1]);
+//});
+
+
+
+//app.use('/', function(req, res, next) {
+//    res.send("Worked!");
+//    next();
+//});
 
 //app.all('/admin/*', doAuthentification);
 //app.get('/admin/stats', returnStats);
 //app.get('/admin/approval', approval);
 
-//functions in the above middelware need to be defined to test this
+//functions in the above middleware need to be defined to test this
 
 //app.get('/test', cb1, cb2);
 
